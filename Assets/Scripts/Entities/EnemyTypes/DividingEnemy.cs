c47bcfa746e03b858740dd9630dde980261e2d4d
Darius Fang <dariusfang1@gmail.com>
2024-05-06 21:26:36 -0600

    Added Dividing Enemy
    for some reason, its kinda laggy, if you change the prefab number of spawn, not sure why .-.

---
+++ Assets/Scripts/Entities/EnemyTypes/DividingEnemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DividingEnemy : Enemy
+{
+    [SerializeField] int numberToSpawn, iterations;
+    private void Awake() {
+        gameObject.SetActive(true);
+    }
+    public override void Die() {
+        if(iterations > 0){
+            StartCoroutine(StartSpawningEnemies());
+        }
+        base.Die();
+    }
+    private void DecreaseIterations(int iterations){
+        this.iterations = iterations-1;
+    }
+    private IEnumerator StartSpawningEnemies(){
+       for (int i = 0; i<numberToSpawn; i++){
+                DividingEnemy splitEnemy = Instantiate(this, transform.position + new Vector3(Mathf.Cos(2*Mathf.PI/numberToSpawn*i), Mathf.Sin(2*Mathf.PI/numberToSpawn*i)), Quaternion.identity);
+                splitEnemy.DecreaseIterations(iterations);
+                
+            }
+        yield return null;
+    }
+}
